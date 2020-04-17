a2b67655388b567d5353a55d0968f22e5261415a
Mikaelluc <Mikael@alu.ufc.br>
2020-04-17 20:48:45 -0300

    Resolvido problema da incompatibilidade list (era só a presença de []). Falta resolver a questão do IndexError caso não insira período de capitalização

--- Teste de rentabilidade.py
+++ Teste de rentabilidade.py
@@ -3,7 +3,7 @@ os.system('clear')
 
 print("Quais investimentos deseja comparar?\n")
 Insves1 = input()
-Capital = input("Capital investido em {}: R$".format(Insves1)).replace(",",".")
+Capital = float(input("Capital investido em {}: R$".format(Insves1)).replace(",","."))
 print("Obs: Em seguida será pedido a taxa de rentabilidade ao mês e ao ano, não é necessário informar ambas pois o calculo é automático. \n")
 a=0
 while (a==0):
@@ -16,12 +16,12 @@ while (a==0):
 
 
 if tax1am == '':
-    tax1am = [(1 + float(tax1aa))**(1/12) - 1]
+    tax1am = (1 + float(tax1aa))**(1/12) - 1
 else:
-    tax1aa = [(1 + float(tax1am))**(12/1) - 1]
+    tax1aa = (1 + float(tax1am))**(12/1) - 1
 
 Tcap = str(input("Período de capitalização: ")).lower().split(' ')
-if Tcap[1]==None or Tcap[1]==IndexError()####################################
+#if Tcap[1]==None or Tcap[1]==IndexError()####################################
 ir = str(input("Cobra IR? ")).lower()
 
 if (Tcap[1]=="meses"):
@@ -32,14 +32,14 @@ else:
     Tcap.append(' a')
     Tcap.replace("a","meses")
 Montante_sem_IR = Capital*(1+float(tax1am))**Tap
-Lucro_depois_do_IR = (Montante - Capital)*0.85
+Lucro_depois_do_IR = (Montante_sem_IR - Capital)*0.85
 Montante_com_IR = Capital + Lucro_depois_do_IR
 
 if (ir == "sim"):
     Montante_final = Capital + Lucro_depois_do_IR
-    print("Valor final: ",Montante_final)
+    print("Valor final: %.2f", %Montante_fina))
 else: 
-    print("Valor final: ",Montante_sem_IR)
+    print("Valor final: %.2f", %Montante_sem_IR)
 
 #Insves2 = input()
 #tax2 = input("Qual a taxa de rentabilidade de {}" .format(Insves2))
